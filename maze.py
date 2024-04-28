d842a8a99c931da7ba4ffe34853db33c5d64d3f2
WindiKRockY <152081374+WindiKRockY@users.noreply.github.com>
2024-04-28 17:30:47 +0300

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 #створи гру "Лабіринт"!
+from random import choice
 from pygame import * #імпортування бібліотеки пайгейм
 
 
@@ -12,12 +13,12 @@ mixer.music.play()
 mixer.music.set_volume(0.5)
 #створи вікно гри
 MAP_WIDTH , MAP_HEIGHT = 25,20 #ширина і висота карти
-TILESIZE = 40 #розмір квадратика карти
+TILESIZE = 30 #розмір квадратика карти
 WIDTH, HEIGHT = MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE 
 
 
 
-WIDTH, HEIGHT = 1100,750
+WIDTH, HEIGHT =  MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE
 
 window = display.set_mode((WIDTH, HEIGHT)) #створення дисплею 
 FPS = 90 #змінна,яка відровідає за частоту кадрів
@@ -42,6 +43,7 @@ class Sprite(sprite.Sprite): #назва класу
         self.rect = self.image.get_rect() #отримання значення
         self.rect.x = x #присвоєння значення x
         self.rect.y = y #присвоєння значення y
+        self.mask = mask.from_surface(self.image)
         all_sprites.add(self)
         
 
@@ -52,6 +54,7 @@ class Player(Sprite):
         self.speed = 2
     def update(self):
         key_pressed = key.get_pressed()
+        old_pos = self.rect.x,self.rect.y
         if key_pressed[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed 
         if key_pressed[K_s] and self.rect.bottom < HEIGHT:
@@ -60,9 +63,40 @@ class Player(Sprite):
             self.rect.x -= self.speed
         if key_pressed[K_d] and self.rect.right < WIDTH :
             self.rect.x += self.speed
+        
+        collide_list = sprite.spritecollide(self,walls,False,sprite.collide_mask)
+        if len(collide_list) > 0:
+            self.rect.x , self.rect.y = old_pos
 
-player = Player(hero,TILESIZE,TILESIZE,300,300)
+
+class Enemy(Sprite):
+    def __init__(self, sprite_img, width, height, x, y):
+        super().__init__(sprite_img, width, height, x, y)
+        # old_pos = self.rect.x,self.rect.y
+        self.damage = 100
+        self.speed = 1
+        self.dir = 'right'
+        self.dir_list = ['right','left','up','down']
+        self.dir = choice(self.dir_list)
+        # enemy_list = sprite.spritecollide(self,walls,False,sprite.collide_mask)
+        # if len(enemy_list) > 0:
+        #     self.rect.x , self.rect.y = old_pos
+
+    def update(self):
+        if self.dir == "right":
+            self.rect.x += self.speed
+        elif self.dir == "left":
+            self.rect.x -= self.speed
+        elif self.dir == "up":
+            self.rect.y += self.speed
+        elif self.dir == "down":
+            self.rect.y -= self.speed
+
+
+player = Player(hero,TILESIZE-5,TILESIZE-5,300,300)
 walls = sprite.Group()
+cyborgs = sprite.Group()
+
 
 with open("map.txt", "r") as f:
     map = f.readlines()
@@ -76,9 +110,11 @@ with open("map.txt", "r") as f:
                 player.rect.x = x
                 player.rect.y = y
             if symbol == "t":
-                treasure = Sprite(treasure,70.70,x,y)
+                treasure = Sprite(treasure,70,70,x,y)
+            if symbol == "e":
+                cyborgs.add(Enemy(cyborg,TILESIZE - 5 , TILESIZE -5 ,x,y))
             
-                x += TILESIZE
+            x += TILESIZE
         y += TILESIZE
         x = 0
         
